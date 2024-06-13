94368a9579dd42e77e4ff0f02fa2012adb0bc958
Mitesh savaliya <msavaliya@plusonex.com>
2024-06-13 09:18:42 +0530

    some config change over lazyvim

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,25 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+-- <c-k> or <c-j> for jump to next/prev error
+
+_G.toggle_relative_number = function()
+  if vim.wo.relativenumber then
+    vim.wo.relativenumber = false
+    vim.wo.number = true
+  else
+    vim.wo.relativenumber = true
+  end
+end
+
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.api.nvim_set_keymap("n", "<leader>r", ":lua toggle_relative_number()<CR>", { noremap = true, silent = true })
